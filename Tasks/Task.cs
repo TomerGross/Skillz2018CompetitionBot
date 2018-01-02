badba763b79fffe4102c825b2fee24c225048fc0
Matan <matanrak@me.com>
2018-01-02 17:35:01 +0200

    Updated Task

--- Tasks/Task.cs
+++ Tasks/Task.cs
@@ -3,13 +3,15 @@
 namespace Punctuation {
 
 	
-	//This is an interface class, it builds the super type to all the goals
+	//This is an interface class, it builds the super type to all the tasks
 	
 	public interface Task {
 		
-		int GetWeight(Pirate pirate); //The per goal cost of chunks
+		int Bias(); //The task's bias
 
-		string Preform(Pirate pirate); //Preform the goal
+		int GetWeight(Pirate pirate); //The per task cost of chunks
+
+		string Preform(Pirate pirate); //Preform the task
 			
 	}
 }
