63dfbe97dbb519e954eb93c4a40c76a5cdcff8aa
yoav-shaham <shaham.yoav2000@gmail.com>
2018-01-02 17:00:47 +0200

    moved the tasks folder

---
+++ Tasks/Task.cs
@@ -0,0 +1,15 @@
+﻿using Pirates;
+
+namespace Punctuation {
+
+	
+	//This is an interface class, it builds the super type to all the goals
+	
+	public interface Task {
+		
+		int GetWeight(Pirate pirate); //The per goal cost of chunks
+
+		string Preform(Pirate pirate); //Preform the goal
+			
+	}
+}
