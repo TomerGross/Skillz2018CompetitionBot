c7cb283c55c10f528137253ba1481c8dfb76b9a5
Tomer Gross <gross.tomer10@gmail.com>
2018-01-24 16:41:40 +0000

    Update TaskBerserker.cs

--- Tasks/TaskBerserker.cs
+++ Tasks/TaskBerserker.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Pirates;
 
-namespace Punctuation {
+namespace Hydra {
 
-/*
 
 	public class TaskBerserker : Task {
 
 		//-------------------Globals---------------------------------------------
-		public static List<Pirate> berserkerDidTurn = new List<Pirate>();
+		
 		public static Dictionary<int,Path> paths = new Dictionary<int,Path>();
 		public PirateGame game = Punctuation.game;
 		//-----------------------------------------------------------------------
@@ -16,56 +15,24 @@ namespace Punctuation {
 	
 		public string Preform(Pirate pirate) {
 
-			if (berserkerDidTurn.Contains(pirate)) {
-				//return ("|Pirate: " + string.Parse(pirate.UniqueId()) + "| Has already did his turn")
-			}
-
-			if (berserkerDidTurn.Contains(pirate)) {
-
-				return "Berserker already did thier turn";
-			}
-
+			
 			if (game.GetEnemyCapsule().Holder != null) {
 
 				Pirate enemyHolder = game.GetEnemyCapsule().Holder;
 
 				if (pirate.CanPush(enemyHolder)) {
-
-					int c = 0;
-					foreach (Pirate pir in game.GetMyLivingPirates()) {
-
-						//if (roles[pir.UniqueId] == Role.BERSERKER) {  //The ROLE system is not used and should not be used anymore
-						if (pir.CanPush(enemyHolder)) {
-							c++;
-						}
-						//}
-					}
-
-					if (c > 1) {
-						foreach (Pirate pir in game.GetMyLivingPirates()) {
-
-							if (roles[pir.UniqueId] == Role.BERSERKER) {
-								if (pir.CanPush(enemyHolder)) {
-									pir.Push(enemyHolder,enemyHolder.Location.Towards(game.GetEnemyMothership(),-5000));
-									berserkerDidTurn.Add(pir);
-								}
-							}
-						}
-					}
-
+					pir.Push(enemyHolder,enemyHolder.Location.Towards(game.GetEnemyMothership(),-5000));
 					return "Berserker pushed enemy holder away.";
-
-				} else {
-
-					berserkerDidTurn.Add(pirate);
+					
+				}
+			    else {
 					pirate.Sail(enemyHolder);
-
 					return "Berserker moving towards enemy holder...";
 				}
 
-			} else {
+			} 
+			else {
 
-				berserkerDidTurn.Add(pirate);
 				pirate.Sail(Punctuation.enemyMine.Towards(Punctuation.myMine,650));
 				return "Berserker moved towards enemy mine.";
 			}
@@ -75,15 +42,20 @@ namespace Punctuation {
 		
 		public int GetWeight(Pirate pirate) {
 
-			// TODO Make the weight algorithm
-
+			if (game.GetEnemyCapsule().Holder != null){
+                List<MapObject> sortedlist = new List<MapObject>();
+				sortedlist = Utils.SoloClosestPair(Main.game.GetMyLivingPirates(), Main.game.GetEnemyCapsule().Holder);
+				int numofpirates = Main.game.GetAllMyPirates().Length;
+				return (numofpirates - sortedlist.IndexOf(pirate)) * (100 / numofpirates);
+			} 
 			return 0;		
 		}
 		
 					
 		public int Bias() {
-			
-			return 0;	
+			if (Main.game.GetEnemyCapsule().Holder() == null)
+			    return 0;
+			return 50;	
 		}
 
 		
@@ -91,5 +63,5 @@ namespace Punctuation {
 		
 	}
 	
-	*/
+	
 }
