47378752fd2a3c4d05fa0f63739041bd8feb1bb3
matanrak <matanrak@me.com>
2018-01-28 18:13:39 +0200

    Created TraitRateByEdges trait and added Utils func to find the distance from the closest edge

---
+++ Pathfinding/Trait/TraitRateByEdges.cs
@@ -0,0 +1,31 @@
+﻿using Pirates;
+
+namespace Hydra {
+
+    public class TraitAttractedToGoal : Trait {
+
+        readonly int range;
+        readonly GameObject goal;
+
+
+        public TraitAttractedToGoal(int range, GameObject goal) {
+
+			this.range = range;
+            this.goal = goal;
+		}
+		
+
+		override public int Cost(Chunk chunk) {
+
+            int MoveDistance = Main.game.PirateMaxSpeed;               
+
+            if (MoveDistance * range > chunk.Distance(goal)){
+                return -5 * (MoveDistance * range - chunk.Distance(goal));
+            }
+
+            return 0; 
+		}
+
+
+	}
+}
