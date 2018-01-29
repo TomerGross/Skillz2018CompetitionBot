e4e687f6116d1dd4a7c7e6301948301f5e5086ba
matanrak <matanrak@me.com>
2018-01-29 15:18:52 +0200

    * PushingTactic.cs: Copied pushing tactic into project from WebKit
    * Hydra.csproj:

---
+++ PushingTactic.cs
@@ -0,0 +1,58 @@
+﻿/*using Pirates;
+
+namespace Hydra
+
+{
+    
+    class PushingTactic
+    {
+        
+        int abletopush = 0;
+        
+        public string Preform(Pirate pirate, TaskType Taskofpirate)
+        {
+            if (Taskofpirate == Task.ESCORT)
+                ImEscort(pirate);
+            else if (Taskofpirate == Task.BERSERKER)
+                ImBerserker(pirate);
+            else if (Taskofpirate == Task.MINER)
+                ImMiner(pirate);
+            else if (Taskofpirate == Task.DEFAULT)
+                ImDefault(pirate);
+            else if (Taskofpirate == Task.MOLE)
+                ImMoler(pirate);
+            else
+                return "never happened";
+        }
+        
+        public void ImEscort(Pirate pirate)
+        {
+            
+        }
+        
+        public void ImBerserker(Pirate pirate)
+        {
+            
+        }
+        
+        public void ImMiner(Pirate pirate)
+        {
+            
+        }
+        
+        public void ImDefault(Pirate pirate)
+        {
+            
+        }
+        
+        public void ImMoler(Pirate pirate)
+        {
+            
+        }
+    }
+    
+    
+    
+    
+    
+}*/
