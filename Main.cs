df70065f0f47b36b172e8121d5ad576d71fb272e
Tomer Gross <gross.tomer10@gmail.com>
2018-02-04 16:30:56 +0000

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -12,7 +12,9 @@ namespace Hydra {
         //---------------[ Main variables ]-----------
         public static PirateGame game;
         public static List<int> didTurn = new List<int>();
-        //public static List<Pirate> enemypirates = game.GetEnemyLivingPirates().ToList(); 
+        public static List<Asteroid> asteroidsPushed = new List<Asteroid>();
+        public static bool debug = false;
+        public static bool importantdebug = false;
         //--------------------------------------------
 
 
@@ -20,6 +22,7 @@ namespace Hydra {
         public static List<Location> mines = new List<Location>();
         public static List<Location> enemyMines = new List<Location>();
         public static int maxMiners = 2;
+        public static int maxMolers = 2;
         //--------------------------------------------
 
 
@@ -27,7 +30,7 @@ namespace Hydra {
         public static Dictionary<int, TaskType> tasks = new Dictionary<int, TaskType>();
         public static List<Pirate> unemployedPirates = new List<Pirate>();
         public readonly List<TaskType> todoTasks = new List<TaskType>(new List<TaskType>
-        { TaskType.MINER, TaskType.ESCORT, TaskType.BOOSTER, TaskType.BERSERKER, TaskType.MOLE});
+        { TaskType.MINER, TaskType.MOLE, TaskType.BERSERKER, TaskType.ESCORT, TaskType.BOOSTER});
         public static int alivePirateCount = 0;
         //--------------------------------------------
 
@@ -38,10 +41,12 @@ namespace Hydra {
 
             // Clearing objects
             numofpushes = 0;
+            asteroidsPushed.Clear();
             tasks.Clear();
             didTurn.Clear();
             alivePirateCount = game.GetMyLivingPirates().Count();
             unemployedPirates = game.GetMyLivingPirates().ToList();
+
 
             // Gettings the mines
             if (game.GetMyCapsules().Count() > 0) {
@@ -51,11 +56,16 @@ namespace Hydra {
             if (game.GetEnemyCapsules().Count() > 0) {
                 game.GetEnemyCapsules().Where(cap => cap.Holder == null && !enemyMines.Contains(cap.Location)).ToList().ForEach(cap => enemyMines.Add(cap.Location));
             }
-
+            
             GiveTasks();
+            
 
             foreach (KeyValuePair<int, TaskType> pair in tasks) {
-                game.Debug(taskTypeToTask(game.GetMyPirateById(pair.Key), pair.Value).Preform());
+                if (!Main.debug) {
+                    taskTypeToTask(game.GetMyPirateById(pair.Key), pair.Value).Preform();
+                } else {
+                    game.Debug(taskTypeToTask(game.GetMyPirateById(pair.Key), pair.Value).Preform());
+                }
             }
         }
 
@@ -74,14 +84,14 @@ namespace Hydra {
                 }
             }
 
-            game.Debug("SCORES LEN: " + scores.Count);
-
-            
             foreach (Pirate pirate in unemployedPirates) {
                 var ptasks = from tup in scores.Keys.ToList().Where(tup => tup.Item1.Id == pirate.Id) select tup.Item2.ToString() + " > " + scores[tup] + "  ||  ";
                 string s = "";
                 ptasks.ToList().ForEach(str => s += str);
-                game.Debug(pirate.Id + " | " + s);
+
+                if (Main.debug) {
+                    game.Debug(pirate.Id + " | " + s);
+                }
             }
 
             return scores;
@@ -100,8 +110,9 @@ namespace Hydra {
 
                 tasks[pirate.Id] = taskType;
                 unemployedPirates.Remove(pirate);
-
-                game.Debug("Gave: " + pirate.Id + " | " + taskType + " at cost: " + scores[sorted.First()]);
+                
+                if (Main.importantdebug)
+                    game.Debug("Gave: " + pirate.Id + " | " + taskType + " at cost: " + scores[sorted.First()]);
             }
         }
 
@@ -114,7 +125,7 @@ namespace Hydra {
 
                 case TaskType.ESCORT:
                     return new TaskEscort(pirate);
-                    
+
                 case TaskType.BOOSTER:
                     return new TaskBooster(pirate);
 
